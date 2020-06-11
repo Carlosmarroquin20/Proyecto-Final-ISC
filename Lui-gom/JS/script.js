9eaa8c94324e5e0cc4b2cc27e0b8e417cafe78a5
luisfergomez <lgomezd4@miumg.edu.gt>
2020-06-11 11:59:35 -0600

    puntos necesarios

--- Lui-gom/JS/script.js
+++ Lui-gom/JS/script.js
@@ -3,5 +3,8 @@ document.getElementById('player').addEventListener("mouseover",sumarPuntos);
 puntos = 0;
 tiempo = 30;
 necesarios = 30;
-
 function sumarPuntos(){
+    puntos++;
+   document.getElementById("puntos").innerHTML = "Puntos: <b>" + puntos + "/" + necesarios + "  </b>";
+   randNum =  Math.round(Math.random()*500);
+   randNum2 =  Math.round(Math.random()*500);
