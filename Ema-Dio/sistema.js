20210771b6e7e08432d8ff500f3a21169213d03b
Emanuel <jdionicioo1@miumg.edu.gt>
2020-06-13 09:42:08 -0600

    se agrego la definicion del mismo

--- Ema-Dio/sistema.js
+++ Ema-Dio/sistema.js
@@ -10,5 +10,44 @@ var Pool = (function()
         return obj;
     };
 
+    //Definicion:
+
+    var def =
+    {
+        _type: null,
+        _size: null,
+        _pointer: null,
+        _elements:null,
+
+        init: function(type, size)
+        {
+            this._type = type;
+            this._size = size;
+            this._pointer = size;
+            this._elements = [];
+
+            var i = 0;
+            var length = this._size;
+
+            for(i; i < length; ++i)
+            {
+                this._elements[i] = this._type.create();
+            }
+        },
+
+        getElement: function()
+        {
+            if(this._pointer > 0) return this._elements[--this._pointer];
+
+            return null;
+        },
+
+        disposeElement: function(obj)
+        {
+            this._elements[this._pointer++] = obj;
+        }
+    };
+
+    return {create:create};
 
 }());
