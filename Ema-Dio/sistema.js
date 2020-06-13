3f4762f63d8dc3612fa2754c63bf2344c5e5e3dd
Emanuel <jdionicioo1@miumg.edu.gt>
2020-06-13 09:41:47 -0600

    se agregaron los metodos expuestos

--- Ema-Dio/sistema.js
+++ Ema-Dio/sistema.js
@@ -1,4 +1,14 @@
 var Pool = (function()
 {
+    //metodos expuestos:
+
+    var create = function(type, size)
+    {
+        var obj = Object.create(def);
+        obj.init(type, size);
+
+        return obj;
+    };
+
 
 }());
