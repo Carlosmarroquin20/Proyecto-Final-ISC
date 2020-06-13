0c1769a3952ccba39ba0b6f909e8bcb3fe1aaa33
Emanuel <jdionicioo1@miumg.edu.gt>
2020-06-13 09:50:47 -0600

    se agrego los metodos expuestos

--- Ema-Dio/sistema.js
+++ Ema-Dio/sistema.js
@@ -304,4 +304,22 @@ var Asteroid = (function()
 var Ship = (function()
 {
 
+    //metodos expuestos:
+
+    var create = function(x, y, ref)
+    {
+        var obj = Object.create(def);
+        obj.ref = ref;
+        obj.angle = 0;
+        obj.pos = Vec2D.create(x, y);
+        obj.vel = Vec2D.create(0, 0);
+        obj.thrust = Vec2D.create(0, 0);
+        obj.idle = false;
+        obj.radius = 8;
+        obj.idleDelay = 0;
+
+        return obj;
+    };
+
+
 }());
